4b7a0423e94d4695ef83fba23649f16f9bdfc410
hungpb13 <hungpb13@gmail.com>
2025-05-20 13:16:46 +0700

    Input

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-# Type Casting = the process of converting a variable from one data type to another
-# str(), int(), float(), bool()
+# input() = a function that prompts user to enter data
+# Return the entered data as a string
 
-name = "Bro code"
-age = 25
-gpa = 3.2
-is_student = True
+name = input("What is your name?: ")
 
-# Float --> Int
-gpa = int(gpa)
-print(gpa)
+age = int(input("How old are you?: "))
+age += 1
 
-# Int --> Float
-age = float(age)
-print(age)
-
-# Int --> Str
-age = str(age)
-age += "1"
-print(age)
-
-# Str --> Bool
-name = bool(name)
-print(name)
-print(bool(""))
-
-# Int --> Bool
-print(bool(1))
-print(bool(0))
+print(f"Hello {name}!")
+print(f"You are {age} years old")
