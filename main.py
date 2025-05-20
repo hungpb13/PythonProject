2ee3051ac57a6516aed7fa6a95556586358d242e
hungpb13 <hungpb13@gmail.com>
2025-05-20 17:26:56 +0700

    Exercise: Countdown Timer

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# For loop = execute a block of code a fixed number of times
-# Iterate over a range, string, sequence, etc.
+# Exercise: Countdown Timer
 
-for i in range(1, 11):
-    print(i, end=" ")
-print("\nReversed range")
-for i in reversed(range(1, 11)):
+import time
+
+for i in reversed(range(1, 6)):
     print(i)
-print("HAPPY NEW YEAR!")
 
-credit_card = "1234-5678-9012-3456"
+my_time = int(input("Enter the time in seconds: "))
 
-for number in credit_card:
-    if number == "-":
-        continue
-    print(number, end=" ")
+for i in range(my_time, 0, -1):
+    seconds = i % 60
+    minutes = int(i / 60) % 60
+    hours = int(i / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP!")
