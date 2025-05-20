21ba817053fd13f56e77d4d5077aaed712b62a33
hungpb13 <hungpb13@gmail.com>
2025-05-20 12:54:24 +0700

    Variable

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-# This is my first Python program
-print("Hello World!")
+# Variable = a container for a value (string, integer, float, boolean)
 
-# Print the result of an expression
-print(1 + 2)
+# String
+first_name = "Bro"
+food = "pizza"
+email = "bro123@gmail.com"
+print(f"Type: {type(email)}")
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
 
-# Print multiple values
-print("Age:", 25)
+# Integer
+age = 25
+quantity = 3
+num_of_students = 30
+print(f"Type: {type(age)}")
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} student(s)")
 
-# Print multiple items
-name = "Alice"
-age = 30
-print("Name:", name, "Age:", age)
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+print(f"Type: {type(price)}")
+print(f"The price is ${price}")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance}km")
 
-# Using f-strings
-print(f"My name is {name} and I am {age} years old.")
+# Boolean = True of False
+# is_student = False
+is_student = True
+print(f"Type: {type(is_student)}")
+print(f"Are you a student?: {is_student}")
 
-# Print with custom seperator or end
-print(name, age, sep=" - ")
-
-# By default, print() ends with a newline \n
-print("Hello", end=" ")
-print("World!")
-
-# Print with escape characters
-print("Line 1\nLine 2") # \n for new line
-print("She said, \"Hello!\"") # \" for quote inside string
+# Use in if statement
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
