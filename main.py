1b51ed6b98f7bb42b4a0498d4915e3fb9530d833
hungpb13 <hungpb13@gmail.com>
2025-05-20 12:34:01 +0700

    Print statement

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 # This is my first Python program
 print("Hello World!")
+
+# Print the result of an expression
+print(1 + 2)
+
+# Print multiple values
+print("Age:", 25)
+
+# Print multiple items
+name = "Alice"
+age = 30
+print("Name:", name, "Age:", age)
+
+# Using f-strings
+print(f"My name is {name} and I am {age} years old.")
+
+# Print with custom seperator or end
+print(name, age, sep=" - ")
+
+# By default, print() ends with a newline \n
+print("Hello", end=" ")
+print("World!")
+
+# Print with escape characters
+print("Line 1\nLine 2") # \n for new line
+print("She said, \"Hello!\"") # \" for quote inside string
