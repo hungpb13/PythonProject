99e69a53a2e598ae1a48f234e444f11729112a5e
hungpb13 <hungpb13@gmail.com>
2025-05-21 21:34:45 +0700

    Membership Operators

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-# *args = pass multiple positional arguments ()
-# **kwargs = pass multiple keyword arguments {key:value}
-# * = unpacking operator
-# Argument: Positional --> Default --> Keyword --> ARBITRARY
+# Membership operator = used to check whether a value or variable is found in a sequence
+# str, list, tuple, set, dict
+# 1. in
+# 2. not in
 
-def add(*args):
-    print(type(args))
-    total = 0
-    for num in args:
-        total += num
-    return total
+def word_guessing_game(word="hello"):
+    playing = True
+
+    while playing:
+        letter = input("Guess a letter in the secret word: ")
+
+        if letter in word:
+            count = word.count(letter)
+            if count == 1:
+                print(f"There is {count} {letter}")
+            else:
+                print(f"There are {count} {letter}")
+        else:
+            print(f"{letter} not found")
+
+        if not input("Continue? (Y/N): ").upper() == "Y":
+            break
+
+    print("Thanks for playing!")
 
 
-print(add(1, 2, 3, 4))
+# word_guessing_game("secret")
+
+def search_student_grade():
+    grades = {
+        "Alice": "C",
+        "Bob": "A",
+        "Charlie": "D",
+        "Danny": "B"
+    }
+
+    student = input("Enter student name: ")
+
+    if student in grades:
+        print(f"{student}: {grades.get(student)}")
+    else:
+        print(f"{student} not found")
 
 
-def print_address(**kwargs):
-    print(type(kwargs))
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
-
-
-print_address(street="123 Fake St.", city="Detroit", state="MI", zip="54321")
+search_student_grade()
