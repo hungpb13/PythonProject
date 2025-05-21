9cf2f0ee3e5fa4bea3e069a87163387d994ec1e4
hungpb13 <hungpb13@gmail.com>
2025-05-21 21:53:09 +0700

    List Comprehension

--- main.py
+++ main.py
@@ -1,45 +1,19 @@
-# Membership operator = used to check whether a value or variable is found in a sequence
-# str, list, tuple, set, dict
-# 1. in
-# 2. not in
+# List comprehension = a concise way to create a list
+# Compact and easier to read than traditional loops
+# [expression for value in iterable if condition]
 
-def word_guessing_game(word="hello"):
-    playing = True
+doubles = [i * 2 for i in range(1, 11)]
 
-    while playing:
-        letter = input("Guess a letter in the secret word: ")
+# for i in range(1, 11):
+#     doubles.append(i * 2)
 
-        if letter in word:
-            count = word.count(letter)
-            if count == 1:
-                print(f"There is {count} {letter}")
-            else:
-                print(f"There are {count} {letter}")
-        else:
-            print(f"{letter} not found")
+print(doubles)
 
-        if not input("Continue? (Y/N): ").upper() == "Y":
-            break
+numbers = [i for i in range(1, 101)]
+print(numbers)
 
-    print("Thanks for playing!")
+even_numbers = [i for i in numbers if i % 2 == 0]
+odd_numbers = [num for num in numbers if num % 2 != 0]
 
-
-# word_guessing_game("secret")
-
-def search_student_grade():
-    grades = {
-        "Alice": "C",
-        "Bob": "A",
-        "Charlie": "D",
-        "Danny": "B"
-    }
-
-    student = input("Enter student name: ")
-
-    if student in grades:
-        print(f"{student}: {grades.get(student)}")
-    else:
-        print(f"{student} not found")
-
-
-search_student_grade()
+print(even_numbers)
+print(odd_numbers)
