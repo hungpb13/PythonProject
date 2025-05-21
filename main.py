0d9ff010958a508ef361f8d8c0b7cab87807b693
hungpb13 <hungpb13@gmail.com>
2025-05-21 12:53:02 +0700

    List

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-# Nested loops = a loop within another loop
-# outer loop:
-#   inner loop:
+# Collection = single "variable" used to store multiple values
+#   List    = [] ordered and changeable. Duplicates OK
+#   Set     = {} unordered and immutable, but Add/Remove OK. NO duplicates
+#   Tuple   = () ordered and unchangeable. Duplicates OK. FASTER
 
-rows = int(input("Enter the number of rows: "))
-columns = int(input("Enter the number of columns: "))
-symbol = input("Enter the symbol to use: ")
+fruits = ["apple", "banana", "orange", "coconut"]
 
-for i in range(rows):
-    for j in range(columns):
-        print(symbol, end=" ")
-    print()
+print(f"Fruits = {fruits}")
+print(f"Fruit[0] = {fruits[0]}")
+print(f"Fruit[0:2] = {fruits[0:2]}")
+print(f"Reversed Fruits = {fruits[::-1]}")
+print(f"Length = {len(fruits)}")
+print(f"Is apple in fruits?: {"apple" in fruits}")
+
+for fruit in fruits:
+    print(fruit)
+
+fruits[0] = "pineapple"
+fruits.append("apple")
+fruits.remove("banana")
+fruits.insert(0, "strawberry")
+fruits.sort(reverse=True)
+fruits.reverse()
+
+print(fruits.index("apple"))
+print(fruits.count("pineapple"))
+
+fruits.clear()
+
+print(fruits)
