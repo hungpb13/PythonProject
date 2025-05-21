1e5a3b16fe2a0ef55be8d444d20a78236a004d18
hungpb13 <hungpb13@gmail.com>
2025-05-21 16:38:07 +0700

    Return Statement

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-# Function = a block of reusable code
-# def function_name():
-#   block of code
-# function_name() --> to invoke it
+# Return = statement used to end a function and send a result back to the caller
 
-def greet(name, age):
-    print(f"Hello, {name}!")
-    print(f"You are {age} years old.")
+def add(a, b):
+    return a + b
 
 
-greet("Bro", 20)
-greet("Steve", 30)
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+a, b = 1, 2
+
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
+print(divide(a, b))
+
+
+def create_name(first_name, last_name):
+    first_name = first_name.capitalize();
+    last_name = last_name.capitalize();
+    return first_name + " " + last_name
+
+
+full_name = create_name("bro", "code")
+print(full_name)
