562f3dcafea722059111abe3fb40343413401f70
hungpb13 <hungpb13@gmail.com>
2025-05-21 13:00:25 +0700

    Tuple

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #   Set     = {} unordered and immutable, but Add/Remove OK. NO duplicates
 #   Tuple   = () ordered and unchangeable. Duplicates OK. FASTER
 
-fruits = {"apple", "banana", "orange", "coconut", "apple"}
+fruits = ("apple", "banana", "orange", "coconut", "apple")
 
 print(f"Fruits = {fruits}")
 
@@ -13,7 +13,7 @@ print(f"Is apple in fruits?: {"apple" in fruits}")
 for fruit in fruits:
     print(fruit)
 
-fruits.add("pineapple")
-fruits.remove("apple")
+print(f"Index of apple: {fruits.index("apple")}")
+print(f"Count of apple: {fruits.count("apple")}")
 
 print(fruits)
