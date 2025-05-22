4a1177399ea15c9b0b04ed998d043885bd5f02e1
hungpb13 <hungpb13@gmail.com>
2025-05-22 11:13:54 +0700

    if __name__ == __main__

--- main.py
+++ main.py
@@ -1,44 +1,11 @@
-# Variable scope = where a variable is visible and accessible
-# Scope resolution = (LEGB) Local --> Enclosed --> Global --> Built-in
-
-# Built-in
-from math import pi
-
-# Global
-global_var = "This is from global"
+# if __name__ == '__main__': = this script can be imported or run standalone
+# Functions and classes in this module can be reused without the MAIN block of code executing
 
 
-# Local
-def function_a():
-    a = 1  # Local
-    print(a)
-    print(global_var)
-    print(pi)
+def main():
+    # Your program goes here
+    print("Hello World!")
 
 
-def function_b():
-    b = 2  # Local
-    print(b)
-    # print(a) --> cannot access variable 'a'
-    print(global_var)
-    pi = 3.14 # Local
-    print(pi)
-
-
-function_a()
-function_b()
-
-
-# Enclosed
-def outer_function():
-    x = "This is from outer"  # Enclosed when used in inner_function()
-
-    def inner_function():
-        x = "This is from inner"  # Local
-        print(x)
-
-    print(x)
-    inner_function()
-
-
-outer_function()
+if __name__ == '__main__':
+    main()
---
+++ script_two.py
@@ -0,0 +1,11 @@
+from script_one import *
+
+
+def favorite_drink(drink):
+    print(f"Your favorite drink is {drink}")
+
+
+print("This is script two")
+favorite_food("pizza")
+favorite_drink("soda")
+print("Goodbye!")
