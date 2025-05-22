1333329600c0c821ffe5287202919e62e0c79b07
hungpb13 <hungpb13@gmail.com>
2025-05-22 12:58:45 +0700

    Exercise: Encryption Program

--- main.py
+++ main.py
@@ -1,77 +1,46 @@
-# Exercise: Slot Machine
+# Exercise: Encryption Program
+
 import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+keys = chars.copy()
+random.shuffle(keys)
+
+print(f"chars: {chars}")
+print(f"keys : {keys}")
 
 
-def spin_row():
-    symbols = ['🍒', '🍉', '🍋', '🔔', '⭐']
+# Encrypt
+def encrypt():
+    plain_text = input("Enter a message to encrypt: ")
+    cipher_text = ""
 
-    return [random.choice(symbols) for _ in range(3)]
+    for letter in plain_text:
+        index = chars.index(letter)
+        cipher_text += keys[index]
 
-
-def print_row(row):
-    print(" | ".join(row))
-
-
-def get_payout(row, bet):
-    if row[0] == row[1] == row[2]:
-        match row[0]:
-            case '🍒':
-                return bet * 2
-            case '🍉':
-                return bet * 3
-            case '🍋':
-                return bet * 4
-            case '🔔':
-                return bet * 5
-            case '⭐':
-                return bet * 10
-    return 0
+    print(f"Original: {plain_text}")
+    print(f"Encrypt : {cipher_text}")
 
 
-def slot_machine(balance=0):
-    print(" Welcome to Slot Machine ")
-    print("---- 🍒 🍉 🍋 🔔 ⭐ ----")
+# Decrypt
+def decrypt():
+    cipher_text = input("Enter a message to decrypt: ")
+    plain_text = ""
 
-    while balance > 0:
-        print(f"Current balance: ${balance:.2f}")
+    for letter in cipher_text:
+        index = keys.index(letter)
+        plain_text += chars[index]
 
-        bet = input("Place your bet: ")
-
-        if not bet.isdigit():
-            print("Please enter a valid number!")
-            continue
-
-        bet = float(bet)
-
-        if bet > balance:
-            print("Insufficient funds!")
-            continue
-        elif bet <= 0:
-            print("Bet must be greater than 0")
-            continue
-
-        row = spin_row()
-        print_row(row)
-
-        payout = get_payout(row, bet)
-
-        if payout > 0:
-            print(f"You won ${payout:.2f}!")
-            balance += payout
-        else:
-            print("You lose this round!")
-            balance -= bet
-
-        play_again = input("Play again? (Press Q to quit): ").upper()
-        if play_again == "Q":
-            break
-
-    print(f"Game over! Your final balance is: ${balance:.2f}")
+    print(f"Encrypt : {cipher_text}")
+    print(f"Original: {plain_text}")
 
 
 def main():
-    balance = 100
-    slot_machine(balance)
+    encrypt()
+    decrypt()
 
 
 if __name__ == "__main__":
