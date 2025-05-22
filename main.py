d744e65f404f4b2064a1ad83ccca7770fde25485
hungpb13 <hungpb13@gmail.com>
2025-05-22 11:48:29 +0700

    Exercise: Banking Program

--- main.py
+++ main.py
@@ -1,11 +1,59 @@
-# if __name__ == '__main__': = this script can be imported or run standalone
-# Functions and classes in this module can be reused without the MAIN block of code executing
+# Exercise: Banking Program
 
+def show_balance(balance):
+    print(f"Your balance is: ${balance:.2f}")
+
+
+def deposit(balance):
+    amount = float(input("Enter an amount to be deposited: "))
+
+    if amount < 0:
+        print("Amount must be greater than 0")
+        return 0
+    else:
+        return amount
+
+
+def withdraw(balance):
+    amount = float(input("Enter an amount to be withdrawn: "))
+
+    if amount < 0:
+        print("Amount must be greater than 0")
+        return 0
+    elif amount > balance:
+        print("Insufficient funds")
+        return 0
+    else:
+        return amount
 
 def main():
-    # Your program goes here
-    print("Hello World!")
+    balance = 0
+    is_running = True
 
+    while is_running:
+        print("----------------------------------")
+        print("         Banking Program          ")
+        print("----------------------------------")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+        print("----------------------------------")
+        choice = input("Enter your choice (1-4): ")
+        print("----------------------------------")
+        match choice:
+            case "1":
+                show_balance(balance)
+            case "2":
+                balance += deposit(balance)
+            case "3":
+                balance -= withdraw(balance)
+            case "4":
+                is_running = False
+            case _:
+                print("That's not a valid choice!")
 
-if __name__ == '__main__':
+    print("Thank you! Have a nice day!")
+
+if __name__ == "__main__":
     main()
