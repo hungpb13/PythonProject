550c0462d3e4b4e9fb2318be32c1141fba92b2ae
hungpb13 <hungpb13@gmail.com>
2025-05-23 20:55:53 +0700

    @property Decorator

--- main.py
+++ main.py
@@ -1,57 +1,56 @@
-# Magic method = dunder method (double underscore) __init__, __str__, __eq__
-# Automatically called by many of Python's built-in operations
-# Allow dev to define or customize the behavior of objects
+# @property = decorator used to define a method as a property
+# Benefit: Add additional logic when read, write, or delete attributes
+# getter, setter, deleter methods
 
-class Book:
-    def __init__(self, title, author, num_pages):
-        self.title = title
-        self.author = author
-        self.num_pages = num_pages
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
 
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+    @property
+    def width(self):
+        return f"{self._width:.1f}cm"
 
-    def __eq__(self, other):
-        return self.title == other.title and self.author == other.author
+    @property
+    def height(self):
+        return f"{self._height:.1f}cm"
 
-    def __lt__(self, other):
-        return self.num_pages < other.num_pages
-
-    def __gt__(self, other):
-        return self.num_pages > other.num_pages
-
-    def __add__(self, other):
-        return self.num_pages + other.num_pages
-
-    def __contains__(self, keyword):
-        return keyword in self.title or keyword in self.author
-
-    def __getitem__(self, key):
-        if key == "title":
-            return self.title
-        elif key == "author":
-            return self.author
-        elif key == "num_pages":
-            return self.num_pages
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
         else:
-            return f"Key {key} was not found"
+            print("Width must be greater than 0")
+
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Height must be greater than 0")
+
+    @width.deleter
+    def width(self):
+        del self._width
+        print("Width has been deleted")
+
+    @height.deleter
+    def height(self):
+        del self._height
+        print("Height has been deleted")
 
 
-book_one = Book("The Lords of the Ring", "J.R.R Tolkien", 1000)
-book_two = Book("Harry Potter", "J.K. Rowling", 800)
-book_three = Book("The Lords of the Ring", "J.R.R Tolkien", 900)
+rectangle = Rectangle(10, 20)
 
-print(book_one)
-print(book_two)
+# Access to a protected member _attribute of a class
+# print(f"Width = {rectangle._width}")
+# print(f"Height = {rectangle._height}")
 
-print(book_one == book_three)
-print(book_two < book_one)
-print(book_two > book_one)
+rectangle.width = 5
+rectangle.height = 10
 
-print(f"Total pages = {book_one + book_two}")
+print(f"Width = {rectangle.width}")
+print(f"Height = {rectangle.height}")
 
-print("Ring" in book_three)
-print("Rowling" in book_two)
-
-print(book_one['num_pages'])
-print(book_one['audio'])
+del rectangle.width
+del rectangle.height
