f8828e44314ffb925b3f7f84323dc6955b92266a
hungpb13 <hungpb13@gmail.com>
2025-05-23 23:40:47 +0700

    Exception

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-# Exercise: Find the Factorial of a Number
-# n! = n x (n-1) x (n-2) x ... x 1
+# Exception = an event that interrupts the flow of a program
+# TypeError, ValueError, ZeroDivisionError
+# try --> except --> finally
 
-# Iterative
-def factorial_i(num):
-    result = 1
-    if num > 0:
-        for i in range(1, num + 1):
-            result *= i
-        return result
-    return result
-
-
-print(factorial_i(2))
-
-
-# Recursive
-def factorial(num):
-    # Base case
-    if num == 1 or num <= 0:
-        return 1
-
-    # Recursive call
-    return num * factorial(num - 1)
-
-
-print(factorial_i(5))
+try:
+    number = int(input("Enter a number: "))
+    print(1 / number)
+except ValueError:
+    print("Enter only number, please!")
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+except Exception: # Too broad exception clause
+    print("Something went wrong!")
+finally:
+    print("Goodbye!")
