4a04c7ef753a782e1a7daac8815d44bc9b7703c8
hungpb13 <hungpb13@gmail.com>
2025-05-23 15:43:24 +0700

    Polymorphism

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-# super() = function used in a child to call methods from a parent class (super class)
-# Extend the functionality of the inherited methods
+# Polymorphism = Greek word that means to "have many forms or faces"
+# Poly = many
+# Morphe = form
+# Inheritance -> override methods in child classes
+
+from abc import ABC, abstractmethod
 
 
-class Shape:
-    def __init__(self, color, is_filled):
-        self.color = color
-        self.is_filled = is_filled
-
-    def describe(self):
-        return f"{self.color} and {'filled' if self.is_filled else 'not filled'}"
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
 
 
 class Circle(Shape):
-    def __init__(self, color, is_filled, radius):
-        super().__init__(color, is_filled)
+    def __init__(self, radius):
         self.radius = radius
 
-    def describe(self):
-        return f"It is a {super().describe()} circle with an area of {3.14 * pow(self.radius, 2)}cm^2"
+    def area(self):
+        return 3.14 * self.radius ** 2
 
 
 class Square(Shape):
-    def __init__(self, color, is_filled, width):
-        super().__init__(color, is_filled)
+    def __init__(self, width):
         self.width = width
 
-    def describe(self):
-        return f"It is a {super().describe()} square with an area of {pow(self.width, 2)}cm^2"
+    def area(self):
+        return self.width ** 2
 
 
 class Rectangle(Shape):
-    def __init__(self, color, is_filled, width, length):
-        super().__init__(color, is_filled)
+    def __init__(self, width, length):
         self.width = width
         self.length = length
 
-    def describe(self):
-        return f"It is a {super().describe()} rectangle with an area of {self.width * self.length}cm^2"
+    def area(self):
+        return self.width * self.length
 
 
-circle = Circle(color="red", is_filled=True, radius=5)
-square = Square(color="blue", is_filled=False, width=10)
-rectangle = Rectangle(color="yellow", is_filled=True, width=10, length=20)
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        super().__init__(radius)
+        self.topping = topping
 
-print(circle.describe())
-print(square.describe())
-print(rectangle.describe())
+
+shapes = [Circle(radius=5), Square(width=5), Rectangle(width=5, length=10), Pizza("pepperoni", 10)]
+
+for shape in shapes:
+    print(f"Area: {shape.area()}cm²")
