4707d28e7b33960afc1ed9a5013b79f891670ce1
hungpb13 <hungpb13@gmail.com>
2025-05-23 17:21:22 +0700

    Composition

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-# Aggregation = represents a relationship where one object (the whole)
-# contains references to one or more INDEPENDENT objects (the parts)
+# Composition = the composed object directly owns its components, which cannot exist independently
+# "owns-a" relationship
 
-class Library:
-    def __init__(self, name):
-        self.name = name
-        self.books = []
-
-    def add_book(self, book):
-        self.books.append(book)
-
-    def list_books(self):
-        return [f"{book.title} by {book.author}" for book in self.books]
+class Engine:
+    def __init__(self, horse_power):
+        self.horse_power = horse_power
 
 
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
+class Wheel:
+    def __init__(self, size):
+        self.size = size
 
 
-library = Library("New York Public Library")
+class Car:
+    def __init__(self, maker, model, horse_power, wheel_size):
+        self.maker = maker
+        self.model = model
+        self.engine = Engine(horse_power)
+        self.wheels = [Wheel(wheel_size) for wheel in range(4)]
 
-book_one = Book("Harry Porter", "J.K. Rowling")
-book_two = Book("The Lords of the Ring", "J.R.R. Tolkien")
+    def display(self):
+        return f"{self.maker} {self.model} with {self.engine.horse_power}(hp) engine and {self.wheels[0].size}(in) wheels"
 
-library.add_book(book_one)
-library.add_book(book_two)
 
-print(library.name)
-for book in library.list_books():
-    print(book)
+car = Car(maker="Ford", model="Mustang", horse_power=500, wheel_size=18)
+print(car.display())
