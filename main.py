5940efaea9243b6eb38d35825dbf0b2a4a8748db
hungpb13 <hungpb13@gmail.com>
2025-05-23 21:38:09 +0700

    map()

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-# Lambda function = a small anonymous function for one time use
-# Take any number of arguments, but have only one expression
-# Keep the namespace clean
-# Useful with higher order functions: sort(), map(), filter(), reduce()
-# lambda parameters: expression
+# map(function, collection) = apply a given function to all items in a collection
 
-double = lambda x: x * 2
-add = lambda x, y: x + y
-max_val = lambda x, y: x if x > y else y
-min_val = lambda x, y: x if x < y else y
-full_name = lambda first, last: first + " " + last
-is_even = lambda x: True if x % 2 == 0 else False
-is_odd = lambda x: True if x % 2 != 0 else False
-is_adult = lambda age: True if age >= 18 else False
+def c_to_f(temp):
+    return (temp * 9 / 5) + 32
 
-print(double(2))
-print(add(1, 2))
-print(max_val(2, 3))
-print(min_val(2, 3))
-print(full_name("Bro", "Code"))
-print(is_even(3))
-print(is_odd(3))
-print(is_adult(30))
+
+celsius_temps = [0.0, 10.0, 20.0, 30.0, 35.0, 40.0]
+
+# fahrenheit_temps = list(map(c_to_f, celsius_temps))
+
+# Lambda
+fahrenheit_temps = list(map(lambda temp: (temp * 9 / 5) + 32, celsius_temps))
+
+print(celsius_temps)
+print(fahrenheit_temps)
