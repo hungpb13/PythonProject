36856515e8439788cca377cfa100e6590ebab9d3
hungpb13 <hungpb13@gmail.com>
2025-05-23 23:04:00 +0700

    Recursion

--- main.py
+++ main.py
@@ -1,98 +1,24 @@
-# Sorting: .sort() and sorted()
-# List[], Tuple(), Dict{key:value}, Object
+# Recursion = a function calls itself from within
+# Help with visualize a complex problem into basic steps which can be solved more easily iteratively or recursively
 
-print("----- List -----")
-fruits = ["banana", "orange", "apple", "pineapple", "coconut"]
-print(fruits)
-
-fruits.sort()
-print(fruits)
-
-fruits.sort(reverse=True)
-print(fruits)
-
-print("----- Tuple -----")
-numbers = (69, 8, 56, 32, 74, 62, 98, 28)
-print(numbers)
-
-# AttributeError: 'tuple' object has no attribute 'sort'
-# numbers.sort()
-
-# sorted(tuple) --> list []
-numbers = sorted(numbers)
-print(numbers)
-
-numbers = tuple(sorted(numbers, reverse=True))
-print(numbers)
-
-print("----- Dict -----")
-students = {
-    "Bob": 7.5,
-    "Daniel": 4.0,
-    "Alice": 8.0,
-    "Emily": 6.5,
-    "Charles": 9.5
-}
-
-print(students)
-
-# Return a sorted list of keys
-# students = sorted(students)
-
-# Return a sorted list of tuple
-# [('Alice', 8.0), ('Bob', 7.5), ('Charles', 9.5), ('Daniel', 4.0), ('Emily', 6.5)]
-# students = sorted(students.items())
-
-# Sort by key
-students = dict(sorted(students.items()))
-print(students)
-
-# Reversed by key
-students = dict(sorted(students.items(), key=lambda item: item[0], reverse=True))
-print(students)
-
-# Sort by value
-students = dict(sorted(students.items(), key=lambda item: item[1]))
-print(students)
-
-# Reverse by value
-students = dict(sorted(students.items(), key=lambda item: item[1], reverse=True))
-print(students)
-
-print("----- Object -----")
+# Iterative
+def walk_i(steps):
+    for step in range(1, steps + 1):
+        print(f"You walk {step} steps")
 
 
-class Car:
-    def __init__(self, model, hp):
-        self.model = model
-        self.hp = hp
+# Recursive
+def walk(steps):
+    # Base case
+    if steps == 0:
+        return
 
-    def __repr__(self):
-        return f"{self.model}: {self.hp}(hp)"
+    # Recursive call
+    walk(steps - 1)
+
+    print(f"You take step #{steps}")
 
 
-cars = [
-    Car("Ford Mustang", 560),
-    Car("BWM i7", 400),
-    Car("Honda Civic", 250),
-    Car("Toyota Camry", 200),
-    Car("Nissan GTR", 600)
-]
+walk_i(5)
+walk(5)
 
-print(cars)
-
-# Sort by model
-cars = sorted(cars, key=lambda car: car.model)
-print(cars)
-
-# Reverse by model
-cars = sorted(cars, key=lambda car: car.model, reverse=True)
-print(cars)
-
-# Sort by hp
-cars = sorted(cars, key=lambda car: car.hp)
-print(cars)
-
-# Reverse by hp
-cars = sorted(cars, key=lambda car: car.hp, reverse=True)
-print(cars)
