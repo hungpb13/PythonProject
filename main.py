0b8dd9b8b60fdbb7c54eabf9756a52075cd5a1d7
hungpb13 <hungpb13@gmail.com>
2025-05-23 22:44:47 +0700

    Sorting

--- main.py
+++ main.py
@@ -1,15 +1,98 @@
-# reduce(function, collection) = reduce elements in a collection to a single value
-# For loop is better in most cases
-# Reduce is better for a functional approach + readability
+# Sorting: .sort() and sorted()
+# List[], Tuple(), Dict{key:value}, Object
 
-from functools import reduce
+print("----- List -----")
+fruits = ["banana", "orange", "apple", "pineapple", "coconut"]
+print(fruits)
 
-grades = [63, 45, 23, 85, 99, 74, 56, 88]
+fruits.sort()
+print(fruits)
 
-add = lambda x, y: x + y
+fruits.sort(reverse=True)
+print(fruits)
 
-total_grade = reduce(add, grades)
+print("----- Tuple -----")
+numbers = (69, 8, 56, 32, 74, 62, 98, 28)
+print(numbers)
 
-average_grade = total_grade / len(grades)
+# AttributeError: 'tuple' object has no attribute 'sort'
+# numbers.sort()
 
-print(f"Average grade = {average_grade:.2f}")
+# sorted(tuple) --> list []
+numbers = sorted(numbers)
+print(numbers)
+
+numbers = tuple(sorted(numbers, reverse=True))
+print(numbers)
+
+print("----- Dict -----")
+students = {
+    "Bob": 7.5,
+    "Daniel": 4.0,
+    "Alice": 8.0,
+    "Emily": 6.5,
+    "Charles": 9.5
+}
+
+print(students)
+
+# Return a sorted list of keys
+# students = sorted(students)
+
+# Return a sorted list of tuple
+# [('Alice', 8.0), ('Bob', 7.5), ('Charles', 9.5), ('Daniel', 4.0), ('Emily', 6.5)]
+# students = sorted(students.items())
+
+# Sort by key
+students = dict(sorted(students.items()))
+print(students)
+
+# Reversed by key
+students = dict(sorted(students.items(), key=lambda item: item[0], reverse=True))
+print(students)
+
+# Sort by value
+students = dict(sorted(students.items(), key=lambda item: item[1]))
+print(students)
+
+# Reverse by value
+students = dict(sorted(students.items(), key=lambda item: item[1], reverse=True))
+print(students)
+
+print("----- Object -----")
+
+
+class Car:
+    def __init__(self, model, hp):
+        self.model = model
+        self.hp = hp
+
+    def __repr__(self):
+        return f"{self.model}: {self.hp}(hp)"
+
+
+cars = [
+    Car("Ford Mustang", 560),
+    Car("BWM i7", 400),
+    Car("Honda Civic", 250),
+    Car("Toyota Camry", 200),
+    Car("Nissan GTR", 600)
+]
+
+print(cars)
+
+# Sort by model
+cars = sorted(cars, key=lambda car: car.model)
+print(cars)
+
+# Reverse by model
+cars = sorted(cars, key=lambda car: car.model, reverse=True)
+print(cars)
+
+# Sort by hp
+cars = sorted(cars, key=lambda car: car.hp)
+print(cars)
+
+# Reverse by hp
+cars = sorted(cars, key=lambda car: car.hp, reverse=True)
+print(cars)
