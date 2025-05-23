1982d577f96af95034bdd6c13131e14384c978d1
hungpb13 <hungpb13@gmail.com>
2025-05-23 17:39:40 +0700

    Nested Class

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-# Composition = the composed object directly owns its components, which cannot exist independently
-# "owns-a" relationship
+# Nested class = a class defined within another class
 
-class Engine:
-    def __init__(self, horse_power):
-        self.horse_power = horse_power
+# class Outer:
+#   class Inner:
+# Benefits:
+# 1. Logically group classes that are closely related
+# 2. Encapsulate private details that aren't relevant outside the outer class
+# 3. Keep namespace clean --> reduce the possibility of naming conflicts
+
+class Company:
+    class Employee:
+        def __init__(self, name, position):
+            self.name = name
+            self.position = position
+
+        def get_details(self):
+            return f"{self.name} - {self.position}"
+
+    def __init__(self, company_name):
+        self.company_name = company_name
+        self.employees = []
+
+    def add_employee(self, name, position):
+        new_employee = self.Employee(name, position)
+        self.employees.append(new_employee)
+
+    def list_employees(self):
+        return [employee.get_details()
+                for employee in self.employees]
 
 
-class Wheel:
-    def __init__(self, size):
-        self.size = size
+company = Company("KFC")
+company.add_employee("Alice", "Manager")
+company.add_employee("Bob", "Cook")
+company.add_employee("Charles", "Cashier")
 
+print(company.company_name)
+print(company.list_employees())
 
-class Car:
-    def __init__(self, maker, model, horse_power, wheel_size):
-        self.maker = maker
-        self.model = model
-        self.engine = Engine(horse_power)
-        self.wheels = [Wheel(wheel_size) for wheel in range(4)]
-
-    def display(self):
-        return f"{self.maker} {self.model} with {self.engine.horse_power}(hp) engine and {self.wheels[0].size}(in) wheels"
-
-
-car = Car(maker="Ford", model="Mustang", horse_power=500, wheel_size=18)
-print(car.display())
+for employee in company.list_employees():
+    print(employee)
