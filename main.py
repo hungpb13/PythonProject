3342a3cf8cfbc6f2955f5712dc7142e89381a044
hungpb13 <hungpb13@gmail.com>
2025-05-26 11:31:48 +0700

    Writing a List of Dictionaries

--- main.py
+++ main.py
@@ -1,65 +1,16 @@
-# Writing files (.txt, .json, .csv)
+# Writing a List of Dictionaries
+
 import csv
-import json
 
-txt_data = "I like pizza!\n"
-
-students = ["Alice", "Bob", "Charles", "Dutch"]
-
-txt_file = "resources/output.txt"
-
-# Mode
-# w = overite
-# a = append
-# x = exclusive creation (try except FileExistsError)
-# r = read (default)
-try:
-    # with statement = automatic file closing
-    with open(file=txt_file, mode="w") as file:
-        file.write(txt_data)
-
-        # Write a list
-        file.write("Students: ")
-        for student in students:
-            file.write(student + " ")
-        print(f"Text file {txt_file} was created")
-except FileExistsError:
-    print("That file already exists")
-
-# Write JSON file
-student = {
-    "name": "Alice",
-    "age": 20,
-    "email": "alice@gmail.com",
-    "gpa": 3.5
-}
-
-json_file = "resources/output.json"
-
-try:
-    with open(json_file, "w") as file:
-        json.dump(student, file, indent=4)
-        print(f"JSON file '{json_file}' was created")
-except FileExistsError:
-    print("That file already exists")
-
-# Write CSV file
 data = [
-    ["Name", "Age", "Email"],  # Header row
-    ["Alice", 25, "alice@example.com"],
-    ["Bob", 30, "bob@example.com"],
-    ["Charlie", 28, "charlie@example.com"]
+    {"Name": "Alice", "Age": 25, "Email": "alice@example.com"},
+    {"Name": "Bob", "Age": 30, "Email": "bob@example.com"}
 ]
+dict_csv_file = "resources/dict.csv"
 
-csv_file = "resources/output.csv"
+with open(dict_csv_file, "w", newline="") as file:
+    fieldnames = ["Name", "Age", "Email"]
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-try:
-    with open(csv_file, "w", newline="") as file:
-        writer = csv.writer(file)
-
-        for row in data:
-            writer.writerow(row)
-
-        print(f"CSV file '{csv_file}' was created")
-except FileExistsError:
-    print("That file already exists")
+    writer.writeheader()  # Writes the header row
+    writer.writerows(data)
