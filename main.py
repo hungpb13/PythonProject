6abe0524428cd9369b5a59519828dd29ba875c76
hungpb13 <hungpb13@gmail.com>
2025-05-26 12:07:32 +0700

    Read File

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# Writing a List of Dictionaries
-
+# Read files (.txt, .json, .csv)
 import csv
+import json
 
-data = [
-    {"Name": "Alice", "Age": 25, "Email": "alice@example.com"},
-    {"Name": "Bob", "Age": 30, "Email": "bob@example.com"}
-]
-dict_csv_file = "resources/dict.csv"
+txt_file = "resources/input.txt"
 
-with open(dict_csv_file, "w", newline="") as file:
-    fieldnames = ["Name", "Age", "Email"]
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
+try:
+    with open(txt_file, "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("File not found")
+except PermissionError:
+    print("Do not have permission to read")
 
-    writer.writeheader()  # Writes the header row
-    writer.writerows(data)
+# JSON file
+json_file = "resources/input.json"
+
+try:
+    with open(json_file, "r") as file:
+        content = json.load(file)
+        print(content)
+
+        # Content = dict
+        # print(content["name"])
+        # for key, value in content.items():
+        #     print(f"{key}: {value}")
+
+        # Content = list
+        for item in content:
+            print(item)
+            for key, value in item.items():
+                print(f"{key}: {value}")
+
+except FileNotFoundError:
+    print("File not found")
+except PermissionError:
+    print("Do not have permission to read")
+except json.decoder.JSONDecodeError:
+    print("Invalid JSON format")
+
+# CSV file
+csv_file = "resources/input.csv"
+
+try:
+    with open(csv_file, "r") as file:
+        # reader = csv.reader(file)
+        # print(reader)
+
+        reader = csv.DictReader(file)
+        fieldnames = reader.fieldnames
+        print(fieldnames)
+        for row in reader:
+            for i in range(len(fieldnames)):
+                fieldname = fieldnames[i]
+                print(f"{fieldname}: {row[fieldname]}", end=" ")
+            print()
+except FileNotFoundError:
+    print("File not found")
+except PermissionError:
+    print("Do not have permission to read")
