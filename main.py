3d0264ee1630b52c23197e50510b72564579af08
hungpb13 <hungpb13@gmail.com>
2025-05-26 13:40:56 +0700

    Exercise: Alarm Clock

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-# Date & Time
-
+# Exercise: Alarm Clock
+import time
 import datetime
 
-date = datetime.date(2025, 1, 1)
-print(f"Date: {date}")
+import pygame
 
-today = date.today()
-print(f"Today: {today}")
 
-time = datetime.time(12, 30, 30)
-print(f"Time: {time}")
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "resources/my_music.mp3"
+    is_running = True
 
-now = datetime.datetime.now()
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
 
-# Format datetime
-pattern = "%H:%M:%S %d/%m/%Y"
-now = now.strftime(pattern)
+        if current_time == alarm_time:
+            print("WAKE UP! ⏰")
 
-print(f"Now: {now}")
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+            is_busy = pygame.mixer.music.get_busy()
 
-target_datetime = datetime.datetime(2020, 1, 1, 12, 0, 0)
-current_datetime = datetime.datetime.now()
+            while is_busy:
+                time.sleep(1)
+                stop = input("Press to stop")
+                if stop is not None:
+                    pygame.mixer.music.stop()
+                    break
 
-if target_datetime < current_datetime:
-    print("Target date has passed")
-else:
-    print("Target date has NOT passed")
+            is_running = False
 
-# Parse String to Date
-date_str = "26-05-2025"
-parsed_date = datetime.datetime.strptime(date_str, "%d-%m-%Y")
-print(f"Parsed date: {parsed_date.strftime(pattern)}")
+        time.sleep(1)
 
-# Doing Date Arithmetic (Timedelta)
-tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
-print(f"Tomorrow: {tomorrow}")
+    print("Have a nice day!")
 
-yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-print(f"Yesterday: {yesterday.strftime(pattern)}")
+
+def main():
+    alarm_time = input("Enter alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
+
+
+if __name__ == "__main__":
+    main()
